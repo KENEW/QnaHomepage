dfd68554da24f2f3a30c9316e486e0f26903c234
KENEW <creeter@naver.com>
2022-06-22 21:22:58 +0900

    조회수 추가

--- pybo/models.py
+++ pybo/models.py
@@ -1,18 +1,36 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-
 class Question(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_question')
+    class Meta:
+        ordering = ['id']
+
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='author_question')
     subject = models.CharField(max_length=200)
     content = models.TextField()
-    create_date = models.DateTimeField()
+    create_date = models.DateTimeField(auto_now_add=True, blank=True)
     modify_date = models.DateTimeField(null=True, blank=True)
-    voter = models.ManyToManyField(User, related_name='voter_question')
+    voter = models.ManyToManyField(CustomUser, related_name='voter_question', blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='category_question')
+    view_count = models.IntegerField(null=True, blank=True, default=0)
+    notice = models.BooleanField(default=False)  # 공지사항 여부
 
     def __str__(self):
         return self.subject
 
+    def get_absolute_url(self):
+        return reverse('pybo:question_detail', args=[self.id])
+
+    def get_recent_comments(self):
+        return self.comment_set.all().order_by('-create_date')[:5]
+
+class Category(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+    description = models.CharField(max_length=200, null=True, blank=True)
+    has_answer = models.BooleanField(default=True)  # 답변가능 여부
+
+    def __str__(self):
+        return self.name
 
 class Answer(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_answer')
--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -1,5 +1,5 @@
 from django.core.paginator import Paginator
-from django.db.models import Q
+from django.db.models import Q, Count
 from django.shortcuts import render, get_object_or_404
 
 from pybo.models import Question
@@ -8,6 +8,17 @@ from pybo.models import Question
 def index(request):
     page = request.GET.get('page', '1')  # 페이지
     kw = request.GET.get('kw', '')  # 검색어
+
+    so = request.GET.get('so', 'recent')  # 정렬기준
+
+    # 정렬
+    if so == 'recommend':
+        question_list = Question.objects.annotate(num_voter=Count('voter')).order_by('-num_voter', '-create_date')
+    elif so == 'popular':
+        question_list = Question.objects.annotate(num_answer=Count('answer')).order_by('-num_answer', '-create_date')
+    else:  # recent
+        question_list = Question.objects.order_by('-create_date')
+
     question_list = Question.objects.order_by('-create_date')
     if kw:
         question_list = question_list.filter(
@@ -19,7 +30,7 @@ def index(request):
         ).distinct()
     paginator = Paginator(question_list, 10)  # 페이지당 10개씩 보여주기
     page_obj = paginator.get_page(page)
-    context = {'question_list': page_obj, 'page': page, 'kw': kw}
+    context = {'question_list': page_obj, 'page': page, 'kw': kw, 'so': so}
     return render(request, 'pybo/question_list.html', context)
 
 
